a1e6a0205faf770046a804893033d28476fb0fb4
Victormath <victormatheusdepaulamonteiro@gmail.com>
2026-03-11 00:36:37 -0300

    duas primeiras aulas
    inicialização das duas primeiras aulas sobre a linguagem C e sua estrutura

---
+++ variáveis.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int idade = 19; //inteiro
+    int quantidade = 1; //inteiro
+    float altura = 1.75; //ponto flutuante
+    float peso = 68; //ponto flutuante
+    char letra = 'VM'; //caractere
+    char nome[20] = "Victor Matheus"; //string com até 20 caracteres
+}
